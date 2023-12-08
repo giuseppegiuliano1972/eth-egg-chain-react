ccfb49338481021ea90306518ece48b22c71920a
waraich1794802 <waraich.1794802@studenti.uniroma1.it>
2023-12-08 19:31:49 +0100

    Fixed TransferEggs and removed unnecessary logs

--- src/gate/gateway.js
+++
@@ -1,7 +0,0 @@
-import web3 from "./web3";
-import Gateway from "../abi/Gateway.json";
-const instance = new web3.eth.Contract(
-  Gateway.abi,
-  "0x5A7085c253BA31484347Bd25796f7fd0B75FFb20"
-);
-export default instance;
