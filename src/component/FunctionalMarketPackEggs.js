811ec926e26b0d6f72ed23badc221323ca44036f
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2024-01-07 20:08:38 +0100

    Added functionalMarketPackEggs.js

---
+++ src/component/FunctionalMarketPackEggs.js
@@ -0,0 +1,101 @@
+import { React, useState } from 'react'
+import "semantic-ui-css/semantic.min.css";
+import { Button, Form, Input, Message } from "semantic-ui-react";
+
+// Kubo and Web3 support
+import { useCommitEgg } from '../hooks/useCommitEgg'
+import { useKubo } from '../hooks/useKubo'
+import { useWeb3 } from '../hooks/useWeb3'
+
+function FunctionalMarketPackEggs() {
+    // state variables
+    const [id, setId] = useState('')
+    const [address, setAddress] = useState('')
+    const [price, setPrice] = useState(0)
+    const [quantity, setQuantity] = useState(0)
+    const [notes, setNotes] = useState('')
+    
+    // personal error
+    const [error, setError] = useState(null)
+
+    // inherited variables
+    const { kuboError, kuboStarting } = useKubo()
+    const { web3Error, web3Starting} = useWeb3()
+    const {
+        loading,
+        cidString,
+        commitMarketEgg,
+    } = useCommitEgg()
+
+    return (
+        <div className="main-container">
+            <h3>Pack Eggs</h3>
+            <Form
+                onSubmit={() => {
+                    setError(null);
+                    commitMarketEgg({
+                        id: id,
+                        address: address,
+                        price: price,
+                        quantity: quantity,
+                        notes: notes
+                    }).catch((error) => setError(error))
+                }}                                                // Creates object egg before committing
+                error={kuboError||web3Error||error}               // Set error
+            >
+                <Form.Field>
+                    <label>Egg id</label>
+                    <Input
+                        value={id}
+                        onChange={(event) => setId(event.target.value)}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label>Market Address</label>
+                    <Input
+                        icon="address card"
+                        iconPosition="left"
+                        value={address}
+                        onChange={(event) => setAddress(event.target.value)}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label>Price</label>
+                    <Input
+                        value={price}
+                        onChange={(event) => setPrice(event.target.value)}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label>How many eggs are in the package?</label>
+                    <Input
+                        value={quantity}
+                        onChange={(event) => setQuantity(event.target.value)}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label>Write any additional notes here</label>
+                    <Input
+                        value={notes}
+                        onChange={(event) => setNotes(event.target.value)}
+                    />
+                </Form.Field>
+                <Message
+                    error
+                    header="There are error/s with your submission"
+                    content={`${error}`}
+                />
+                <Button color="teal" loading={kuboStarting||web3Starting||loading}>
+                    Pack
+                </Button>
+                {(cidString !== '' && error === null) ? <Message
+                    positive
+                    header="You packaged an egg!"
+                    content={`CID: ${cidString}`}
+                /> : null}
+            </Form>
+        </div>
+    )
+}
+
+export default FunctionalMarketPackEggs
--- src/hooks/useCommitEgg.js
+++ src/hooks/useCommitEgg.js
@@ -49,6 +49,42 @@ export const useCommitEgg = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gateway, web3Error, web3Starting, kuboError, kuboStarting, kubo])
 
+  const commitMarketEgg = useCallback(async (json) => {
+    if (!kuboError && !kuboStarting && !web3Error && !web3Starting) {
+      try {
+        setLoading(true);
+
+        // TODO: Add input validation
+
+        // add egg as a dag json to ipfs
+        const cid = await kubo.dag.put(json);
+
+        // convert to string and set cid
+        setCidString(cid.toString())
+
+        // register egg and handle outcome
+        console.log(cid)
+        await gateway.methods.packMarketEgg(json.address, web3.utils.bytesToHex(cid.multihash.digest))
+                                .send({from: json.address})
+                                .on('confirmation', function(confirmation, receipt){
+                                  // Put here any feedback on transaction result
+                                  console.log("Transaction confirmed!");
+                                })
+                                .on('error', function(error, receipt){
+                                  console.error(error);
+                                });
+
+        // Handle outcome, for now a simple log
+        console.log('Added egg with CID:', cid.toString())
+      } finally {
+        setLoading(false)
+      }
+    } else {
+      console.log('please wait for kubo and web3 to start')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gateway, web3Error, web3Starting, kuboError, kuboStarting, kubo])
+
   const fetchCommittedEgg = useCallback(async () => {
     if (!kuboError && !kuboStarting && !web3Error && !web3Starting) {
       try {
@@ -90,5 +126,5 @@ export const useCommitEgg = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gateway, web3Error, web3Starting, kubo, kuboError, kuboStarting, cidString])
   
-  return { loading, cidString, setCidString, committedEgg, commitEgg, fetchCommittedEgg }
+  return { loading, cidString, setCidString, committedEgg, commitEgg, commitMarketEgg, fetchCommittedEgg }
 }
